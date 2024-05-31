562ebc2ec9bb37c910d71a7ae5550600d8358521
dillthe <seulgi0908@gmail.com>
2024-05-31 00:26:59 -0400

    feat: Add validation constraints to ParticipantEntity

--- src/main/java/com/github/secretsanta/repository/entity/ParticipantEntity.java
+++ src/main/java/com/github/secretsanta/repository/entity/ParticipantEntity.java
@@ -1,6 +1,8 @@
 package com.github.secretsanta.repository.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +21,13 @@ public class ParticipantEntity {
     @Column(name = "participant_id")
     private Integer participantId;
 
+    @NotBlank(message = "Participant name cannot be blank")
+    @Pattern(regexp = "^[가-힣a-zA-Z\\s'-]+$", message = "Participant name should be valid")
     @Column(name = "participant_name", nullable = false)
     private String participantName;
 
+    @NotBlank(message = "Email cannot be blank")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "Email should be valid")
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 }
--- src/main/java/com/github/secretsanta/web/dto/ParticipantDTO.java
+++ src/main/java/com/github/secretsanta/web/dto/ParticipantDTO.java
@@ -2,6 +2,9 @@ package com.github.secretsanta.web.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 @Getter
 @Setter
@@ -10,7 +13,17 @@ import lombok.*;
 @ToString
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ParticipantDTO {
+        @Schema(description = "Participant Id", example = "1")
         private Integer participantId;
+
+        @Schema(description = "Participant Name", example = "John Chris, 박나래 : 영문 및 한글 이름 입력 가능")
+        @NotBlank(message = "Participant name cannot be blank")
+        @Pattern(regexp = "^[가-힣a-zA-Z\\s'-]+$", message = "Participant name should be valid")
         private String participantName;
+
+        @Schema(description = "Email", example = "abc@gmail.com, 이메일 형식으로 입력해주세요")
+        @NotBlank(message = "Email cannot be blank")
+//    @Email(message = "Email should be valid")
+        @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "Email should be valid")
         private String email;
 }
